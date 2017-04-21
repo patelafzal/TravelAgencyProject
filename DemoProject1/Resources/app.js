adf1b8808f02806bdc191c17eca89507a5900006
Afzal Patel <afzal.patel09@outlook.com>
2017-04-22 01:58:58 +0530

    Some changes

---
+++ DemoProject1/Resources/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+
+    angular.module('TravellersApp', ['ngRoute']).config(config);
+
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/Views/Index',
+                controller: 'HomeController'
+            });
+
+        $locationProvider.html5Mode(true);
+    }
+
+})();
